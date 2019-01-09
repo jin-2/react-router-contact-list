4b4e032898c281876a0f6f92f830459f2f0889f0
jin-2 <jmakeaz@gmail.com>
2019-01-09 20:42:00 +0900

    #3-10 Contact List App- Contacts Routes and Contact Detail

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 
 const App = () => (
   <div>
@@ -10,6 +10,12 @@ const App = () => (
 const Header = () => (
   <header>
     <h1>My Contact</h1>
+    <nav>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/contacts">Contact</Link></li>
+      </ul>
+    </nav>
     <Route exact path="/" component={Welcome} />
     <Route path="/contacts" component={Contacts} />
   </header>
@@ -19,14 +25,26 @@ const Welcome = () => (
   <h1>welcome!!</h1>
 );
 
-const Contacts = () => (
-  <ul>
-    <li>강아지</li>
-    <li>고양이</li>
-    <li>코끼리</li>
-  </ul>
+const Contacts = ({ match }) => (
+  <div>
+    <ul>
+      <li><Link to={`${match.url}/dog`}>강아지</Link></li>
+      <li><Link to={`${match.url}/cat`}>고양이</Link></li>
+      <li><Link to={`${match.url}/elephant`}>코끼리</Link></li>
+    </ul>
+    <Route path={`${match.path}/:contactName`} component={Contact} />
+    <Route
+      exact
+      path={match.path}
+      render={() => <p>Please select a contact.</p>}
+    />
+  </div>
 );
 
-
+const Contact = ({ match }) => (
+  <div>
+    <div>contact detail for {match.params.contactName}</div>
+  </div>
+);
 
 export default App;
